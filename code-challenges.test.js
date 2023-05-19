ceaf6828aac7bbee3599a4cd98df7c14024f3e18
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-05-19 19:19:27 -0400

    One code left plus interview questions

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,6 +13,20 @@
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
+describe("codeMessage", () => {
+    it("takes in a string and returns a string with a coded message.", () => {
+        expect(codeMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+        expect(codeMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect(codeMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
+// FAIL  ./code-challenges.test.js
+// codeMessage
+//   ✕ takes in a string and returns a string with a coded message. (1 ms)
+
+// ● codeMessage › takes in a string and returns a string with a coded message.
+
+//   ReferenceError: condeMessage is not defined
 
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
@@ -22,10 +36,47 @@ const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 // b) Create the function that makes the test pass.
+// Pseudocode:
+// Input: A string
+// Output: A string containing a coded message with certain characters changed to the appropriate number
+// Process:
+// Create a function called codeMessage that takes in a string as an argument
+// in order to gain access to each character we can split the string into an array and iterate through it using map.
+// While iterating through it use a conditional to check if the value is equal to a, e, i, o.
+// Since it is case sensitive we can convert each value to a lowercase with the toLowerCase built in method.
+// The conditionals will swap out the appropriate characters to numbers.
+// We then can return the expected output with join method to revert back to a string. 
+
+const codeMessage = (str) => {
+    return str.split("").map((value) => 
+                value.toLowerCase() === "a" ?  4
+                : value.toLowerCase() === "e" ?  3
+                : value.toLowerCase() === "i" ?  1
+                : value.toLowerCase() === "o" ?  0
+                : value
+    ).join("")
+}
+// PASS  ./code-challenges.test.js
+// codeMessage
+//   ✓ takes in a string and returns a string with a coded message. (3 ms)
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
+describe("certainWords" , () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter.", () => {
+        expect(certainWords(fruitArray, filterLetterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(certainWords(fruitArray, filterLetterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
+// FAIL  ./code-challenges.test.js
+// certainWords
+//   ✕ takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
+
+// ● certainWords › takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
+
+//   ReferenceError: certainWords is not defined
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -35,10 +86,44 @@ const filterLetterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // b) Create the function that makes the test pass.
+// PsuedoCode
+// Input: An array of words as well as a single letter
+// Output: An new array of words that only contain the specified letter
+// Process:
+// Create a function called certainWords that takes in an array and a string as arguments
+// Since we will be returning a subset of the original array of different length we can iterate with the filter method. 
+// As this will be case sensitive we can us the lowercase built in method with the value of each index, as well as the includes method to decide if that value includes the specified 
+
+
+
+
+const certainWords = (arr, str) => {
+    return arr.filter(value => value.toLowerCase().includes(str.toLowerCase()) 
+    )
+}
+// PASS  ./code-challenges.test.js
+// certainWords
+//   ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter. (1 ms)
+
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+describe("fullHouse", () => {
+    it("determines whether or not the array is a full house.", () => {
+        expect(fullHouse(hand1)).toEqual(true)
+        expect(fullHouse(hand2)).toEqual(false)
+        expect(fullHouse(hand3)).toEqual(false)
+        expect(fullHouse(hand4)).toEqual(true)
+    })
+})
+// FAIL  ./code-challenges.test.js
+// fullHouse
+//   ✕ determines whether or not the array is a full house.
+
+// ● fullHouse › determines whether or not the array is a full house.
+
+//   ReferenceError: fullHouse is not defined
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -50,3 +135,14 @@ const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+
+const fullHouse = (arr) => {
+    
+}
+
+
+
+
+
+
+// Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,22 @@ filter_letter_o = 'o'
 filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+def certain_letter(array,letter)
+    new_arr = []
+
+    array.each do |word|
+        if word.include?(letter)
+            new_arr << word
+        end
+    end
+    p new_arr
+end
+
+# OUTPUTS
+# certain_letter(beverages_array, filter_letter_o)
+# Output: ["coffee", "soda water"]
+# certain_letter(beverages_array, filter_letter_t)
+# Output: ["tea", "water", "soda water"]
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -17,11 +33,63 @@ filter_letter_t = 't'
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+def alph_states(hash)
+    
+    p hash.values.flatten.sort
+end  
+
+# OUTPUTS
+# alph_states(us_states)
+# Output:["Arizona", "California", "Idaho", "Maine", "Nevada", "New Hampshire", "Oregon", "Rhode Island", "Washington"]
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+class BikeStuff
+    attr_accessor :model, :wheels, :current_speed, :speed, :break_amt
+    def initialize(model, wheels = 2)
+        @model = model
+        @wheels = wheels
+        @current_speed = 0
+    end
+    
+    def pedal_faster speed
+         @current_speed += speed
+    end
 
+    def break break_amt
+        @current_speed -= break_amt
+        if @current_speed > 0
+             @current_speed 
+        else
+             @current_speed = 0
+        end
+    end
+
+    def bike_info
+       p "The #{model} bike has #{wheels} wheels and is going #{current_speed} mph."
+    end
+end
+
+
+
+# OUTPUTS:
+# my_bike = BikeStuff.new('Trek')
+# my_bike.bike_info
+# my_bike.pedal_faster(10)
+# my_bike.bike_info
+# my_bike.pedal_faster(18)
+# my_bike.bike_info
+# my_bike.break(5)
+# my_bike.bike_info
+# my_bike.break(25)
+# my_bike.bike_info
+
+# "The Trek bike has 2 wheels and is going 0 mph."
+# "The Trek bike has 2 wheels and is going 10 mph."
+# "The Trek bike has 2 wheels and is going 28 mph."
+# "The Trek bike has 2 wheels and is going 23 mph."
+# "The Trek bike has 2 wheels and is going 0 mph."
 
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
@@ -30,3 +98,6 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+
+# COMBINED WITH PART A ^^^^^^^^^^^
